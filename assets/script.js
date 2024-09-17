90e6232f44356d0344a1b5b932b695b23a5f3d25
Guylène D <165901249+guylened@users.noreply.github.com>
2024-09-17 16:19:29 +0200

    carrousel V1

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,31 @@
+const buttons = document.querySelectorAll(".arrow");
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
 
-const arrowLft = document.getElementById("arrowlft");
-arrowLft.addEventListener("click", myFunction1);
+buttons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {		
+		const calcNextSlide = e.target.id === "next" ? 1 : -1;
+		const SlideActive = document.querySelector(".slide-selected");		
+		const DotActive = document.querySelector(".dot-selected");
+		NewSlideIndex = calcNextSlide + [...slides].indexOf(SlideActive);
+		NewDotIndex = calcNextSlide + [...dots].indexOf(DotActive);
 
-function myFunction1() {
-  document.getElementById("banner").innerHTML = "Hello World";
-}
+		if(NewSlideIndex < 0) NewSlideIndex = [...slides].length - 1;
+		if(NewSlideIndex >= [...slides].length) NewSlideIndex = 0
+		slides[NewSlideIndex].classList.add("slide-selected");
+		SlideActive.classList.remove("slide-selected");
 
-const arrowRgt = document.getElementById("arrowrgt");
-arrowRgt.addEventListener("click", myFunction2);
+		if(NewDotIndex < 0) NewDotIndex = [...slides].length - 1;
+		if(NewDotIndex >= [...slides].length) NewDotIndex = 0
+		dots[NewDotIndex].classList.add("dot-selected");
+		DotActive.classList.remove("dot-selected");
+		
+		
 
-function myFunction2() {
-  document.getElementById("banner").innerHTML = "Hello World";
-}
+		
+	})
 
 
+})
 
 
-
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
