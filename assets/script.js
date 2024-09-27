997b57e19860ae5cb7bfc3d1a51c23882ccfc827
Guylène D <165901249+guylened@users.noreply.github.com>
2024-09-27 11:49:26 +0200

    V4

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,78 @@
-const slides = document.querySelectorAll(".slide");
+const imgSlide = document.getElementById("imgslide");
+const pSlide = document.getElementById("pslide");
 const dots = document.querySelectorAll(".dot");
-const btnPrev = document.getElementById("prev")
-const btnNext = document.getElementById("next")
-const dot1 = document.getElementById("dot1")
-const dot2 = document.getElementById("dot2")
-const dot3 = document.getElementById("dot3")
-const dot4 = document.getElementById("dot4")
-let slideActive = document.querySelector(".slide-selected");		
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
+const slides = [
+  {
+    image: "./assets/images/slideshow/slide1.jpg",
+    description:
+      "<p>Impressions tous formats <span>en boutique et en ligne</span></p>",
+  },
+  {
+    image: "./assets/images/slideshow/slide2.jpg",
+    description:
+      "<p>Tirages haute définition grand format <span>pour vos bureaux et events</span></p>",
+  },
+  {
+    image: "./assets/images/slideshow/slide3.jpg",
+    description:
+      "<p>Grand choix de couleurs <span>de CMJN aux pantones</span></p>",
+  },
+  {
+    image: "./assets/images/slideshow/slide4.png",
+    description:
+      "<p>Autocollants <span>avec découpe laser sur mesure</span></p>",
+  },
+];
+
 let dotActive = document.querySelector(".dot-selected");
-
-
-// initialisation du compteur de l'index
 let indexCurrent = 0;
-let slidesLength = slides.length -1;
+let slidesLength = slides.length - 1;
 
-function updateSlides() {
-	slideActive.classList.remove("slide-selected");
-	dotActive.classList.remove("dot-selected");
-	slides[indexCurrent].classList.add("slide-selected");
-	dots[indexCurrent].classList.add("dot-selected");
+function updateDots() {
+  dotActive.classList.remove("dot-selected");
+  dots[indexCurrent].classList.add("dot-selected");
 }
 
+function updateSlides() {
+  pSlide.innerHTML = slides[indexCurrent].description;
+  imgSlide.src = slides[indexCurrent].image;
+}
 
+function prevSlide() {
+  dotActive = document.querySelector(".dot-selected");
+  indexCurrent--;
+  if (indexCurrent < 0) {
+    indexCurrent = slidesLength;
+  }
 
-// trigger slide précédente
-btnPrev.addEventListener("click", function() {
-	slideActive = document.querySelector(".slide-selected");		
-	dotActive = document.querySelector(".dot-selected");
+  updateSlides();
+  updateDots();
+}
 
-	indexCurrent--;
-	
-	if (indexCurrent < 0) {
-		indexCurrent = slidesLength;
-	}
+function nextSlide() {
+  dotActive = document.querySelector(".dot-selected");
+  indexCurrent++;
+  if (indexCurrent > slidesLength) {
+    indexCurrent = 0;
+  }
 
-	updateSlides();
-	
-})	
+  updateSlides();
+  updateDots();
+}
 
-// trigger slide suivante
-btnNext.addEventListener("click", function() {
-	slideActive = document.querySelector(".slide-selected");		
-	dotActive = document.querySelector(".dot-selected");
+btnNext.addEventListener("click", nextSlide);
 
-	indexCurrent++;
-	
-	if (indexCurrent > slidesLength) {
-		indexCurrent = 0 ;
-	}
-	
-	updateSlides();
-	
-})
-
+btnPrev.addEventListener("click", prevSlide);
 
 //trigger sur les dots
-dot1.addEventListener("click", function() {
-	slideActive = document.querySelector(".slide-selected");		
-	dotActive = document.querySelector(".dot-selected");
-	indexCurrent = [0]
-	updateSlides();
-})
 
-dot2.addEventListener("click", function() {
-	slideActive = document.querySelector(".slide-selected");		
-	dotActive = document.querySelector(".dot-selected");
-	indexCurrent = [1]
-	updateSlides();
-})
-
-dot3.addEventListener("click", function() {
-	slideActive = document.querySelector(".slide-selected");		
-	dotActive = document.querySelector(".dot-selected");
-	indexCurrent = [2]
-	updateSlides();
-})
-
-dot4.addEventListener("click", function() {
-	slideActive = document.querySelector(".slide-selected");		
-	dotActive = document.querySelector(".dot-selected");
-	indexCurrent = [3]
-	updateSlides();
-})
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", function () {
+    dotActive = document.querySelector(".dot-selected");
+    indexCurrent = [index];
+    updateSlides();
+    updateDots();
+  });
+});
